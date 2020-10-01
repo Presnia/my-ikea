c3e2b25744052061d926bc2b61c5d677312be139
Andrei Dzebysh <presnia@yahoo.com>
2020-10-01 20:13:05 +0300

    Finished 2-nd day

--- script/loadData.js
+++ script/loadData.js
@@ -1,17 +1,21 @@
+import {
+  getData
+} from './getData.js'
+
 const wishList = ['idd007', 'idd008', 'idd015', 'idd023', 'idd034'];
 
 const cartList = [{
-    id: 'idd11',
-    count: 3,
+    id: 'idd011',
+    count: 3
   },
   {
-    id: 'idd22',
-    count: 1,
+    id: 'idd022',
+    count: 1
   },
   {
-    id: 'idd33',
-    count: 2,
-  },
+    id: 'idd033',
+    count: 2
+  }
 ];
 
 export const loadData = () => {
@@ -19,14 +23,24 @@ export const loadData = () => {
 
   if (location.search) {
     const search = decodeURI(location.search);
-    console.log('search');
     const prop = search.split('=')[0].substring(1);
     const value = search.split('=')[1];
+
+
+    if (prop === 's') {
+      getData.search(value, (data) => console.log(data));
+    } else if (prop === 'wishlist') {
+      getData.wishList(wishList, (data) => console.dir({
+        wishlist: data
+      }));
+    } else {
+      getData.category(prop, value, (data) => console.log(data));
+    }
   }
   if (location.hash) {
-    console.log('hash');
+    getData.item(location.hash.substring(1), (data) => console.log(data));
   }
   if (location.pathname.includes('cart')) {
-    console.log('cart');
+    getData.cart(cartList, (data) => console.log(data));
   }
 };
